7de8611fd9de546084cc9cbecff5afe1d2910d91
M.T <masaki-takeshita@MTMacBook-Pro.local>
2024-10-20 14:31:06 +0900

    仮のmysql作成

---
+++ docker/mysql/sql/create_table_gender.sql
@@ -0,0 +1,8 @@
+create table IF not exists `gender_table`
+(
+ `id`               INT(2) AUTO_INCREMENT,
+ `gender`           VARCHAR(3) NOT NULL,
+ `created_at`       Datetime DEFAULT CURRENT_TIMESTAMP,
+ `updated_at`       Datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
