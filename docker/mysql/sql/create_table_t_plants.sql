438e585aa20818009e4400764e6c55927a202edb
M.T <masaki-takeshita@MTMacBook-Pro.local>
2024-10-20 19:14:25 +0900

    植物水やり君用テーブル構成作成

--- docker/mysql/sql/create_table_t_plants.sql
+++ docker/mysql/sql/create_table_t_plants.sql
@@ -1,8 +1,10 @@
-create table IF not exists `gender_table`
+create table IF not exists `t_plants`
 (
  `id`               INT(2) AUTO_INCREMENT,
- `gender`           VARCHAR(3) NOT NULL,
+ `name`             VARCHAR(20) NOT NULL,
+ `place_id`         INT(2) NOT NULL,
+ `frequency`        INT(3) NOT NULL,
  `created_at`       Datetime DEFAULT CURRENT_TIMESTAMP,
  `updated_at`       Datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`,`name`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- docker/mysql/sql/insert_table_gender.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO gender_table(id,gender) VALUES (1,"男性");
-INSERT INTO gender_table(id,gender) VALUES (2, "女性");
-INSERT INTO gender_table(id,gender) VALUES (3, "その他");
